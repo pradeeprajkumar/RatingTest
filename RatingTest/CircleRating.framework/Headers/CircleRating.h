cee86163b69c01e782e55f7cd54c36a9602a5531
Pradeep Rajkumar <>
2018-04-18 23:03:04 +0530

    Initial commit.

---
+++ RatingTest/CircleRating.framework/Headers/CircleRating.h
@@ -0,0 +1,19 @@
+//
+//  CircleRating.h
+//  CircleRating
+//
+//  Created by Pradeep Rajkumar on 17/04/18.
+//  Copyright © 2018 Pradeep Rajkumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CircleRating.
+FOUNDATION_EXPORT double CircleRatingVersionNumber;
+
+//! Project version string for CircleRating.
+FOUNDATION_EXPORT const unsigned char CircleRatingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircleRating/PublicHeader.h>
+
+
---
+++ RatingTest/RatingTest/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RatingTest
+//
+//  Created by Pradeep Rajkumar on 18/04/18.
+//  Copyright © 2018 Pradeep Rajkumar. All rights reserved.
+//
+
+import UIKit
+import CircleRating
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let customRatingView = Bundle.init(for: CircleRatingView.self).loadNibNamed("CircleRatingView", owner: self, options: nil)?.first as? CircleRatingView {
+            customRatingView.setupRatingView(numberOfCircles: 5, numberOfFilled: 4)
+            customRatingView.frame = CGRect(x: 30, y: 50, width: 44, height: 9)
+            self.view.addSubview(customRatingView)
+        }
+    }
+}
+
